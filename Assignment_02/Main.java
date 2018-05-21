73dfc4c9a30d53ef1e5e921ad11bc3f0ebab6464
JuanWeimar <juan_evil_92@hotmail.com>
2018-05-21 17:38:14 +0200

    Second Assignment
    QuickShort
    Made with LinkedList primitive Java class

---
+++ Assignment_02/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int[] list = {1, 4, 8, 13, 33, 40, 67, 83, 98, 112, 121};
+        
+
+
+
+    }
+
+
+    public void QuickShort(int[] list){
+        Random rnd = new Random();
+        int pivot = rnd.nextInt(50);
+
+    }
+}
