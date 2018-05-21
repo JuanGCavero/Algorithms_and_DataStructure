e9e5fe08fa01605347544bba0ae051f4232dc4b4
JuanWeimar <juan_evil_92@hotmail.com>
2018-05-21 17:44:22 +0200

    A2 Complete

--- Assignment_02/Main.java
+++ Assignment_02/Main.java
@@ -1,18 +1,75 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+// Class Main
 public class Main {
 
-    public static void main(String[] args) {
-        
-        int[] list = {1, 4, 8, 13, 33, 40, 67, 83, 98, 112, 121};
-        
+	public static void main(String[] args) {
+		System.out.println("Juan González Cavero, 118928");
+		System.out.println("Digital Engineering");
 
+		LinkedList<Integer> lkList = new LinkedList<Integer>();
+		// Arbitrary numbers
+		lkList.add(83);
+		lkList.add(21);
+		lkList.add(121);
+		lkList.add(8);
+		lkList.add(4);
+		lkList.add(40);
+		lkList.add(112);
+		lkList.add(1);
+		lkList.add(98);
+		lkList.add(67);
+		lkList.add(33);
 
+		LinkedList<Integer> lkShorted = new LinkedList<Integer>();
 
-    }
+		lkList = QuickShort(lkList, lkShorted);
 
+		for (Integer number : lkList) {
+			System.out.println(number);
+		}
+	}
 
-    public void QuickShort(int[] list){
-        Random rnd = new Random();
-        int pivot = rnd.nextInt(50);
+	public static LinkedList<Integer> QuickShort(LinkedList<Integer> lkList, LinkedList<Integer> lkShorted) {
 
-    }
+		// Choose an element randomly, "Pivot"
+		Random rnd = new Random();
+		int pivot = rnd.nextInt(lkList.size());
+
+		// Elements smaller than Pivot go to the left,
+		// bigger ones to the right.
+		LinkedList<Integer> lkLeft = new LinkedList<Integer>();
+		LinkedList<Integer> lkRight = new LinkedList<Integer>();
+		for (Integer number : lkList) {
+			if (number < lkList.get(pivot)) {
+				lkLeft.addFirst(number);
+			}
+			if (number > lkList.get(pivot)) {
+				lkRight.addFirst(number);
+			}
+		}
+
+		// I already have left and right sets
+		if (!lkLeft.isEmpty()) {
+			// Break if in the set are two or less Elements
+			if (lkLeft.size() == 1)
+				lkShorted.add(lkLeft.getFirst());
+			// Do the procedure recursively
+			else {
+				QuickShort(lkLeft, lkShorted);
+			}
+		}
+		lkShorted.add(lkList.get(pivot));
+		if (!lkRight.isEmpty()) {
+			// Break if in the set are two or less Elements
+			if (lkRight.size() == 1)
+				lkShorted.add(lkRight.getFirst());
+			// Do the procedure recursively
+			else {
+				QuickShort(lkRight, lkShorted);
+			}
+		}
+		return lkShorted;
+	}
 }
